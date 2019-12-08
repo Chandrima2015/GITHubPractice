3fd08b12adf0f5c3050139dd52e2f63ce5d3a56a
Chandrima2015 <58661372+Chandrima2015@users.noreply.github.com>
2019-12-08 22:00:19 +0530

    Create Test3.java

---
+++ Test3.java
@@ -0,0 +1,11 @@
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Test3.main()");
+		System.out.println("Test3.main()updated");
+	}
+
+}
