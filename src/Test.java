403005eade65c9a69afff7e0df6fc67b06820516
Pratik <Pratik@MukherjeeDomain>
2019-12-08 21:04:50 +0530

    New File added

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Pratik
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Test.main()");
+	}
+
+}
